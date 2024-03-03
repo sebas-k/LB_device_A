f61785cce0ff38fb4504a095137b10ba1251e81d
sebas-k <116971160+sebas-k@users.noreply.github.com>
2024-03-03 16:07:43 +0100

    Commit

--- main/LB_device_A.c
+++ main/LB_device_A.c
@@ -14,6 +14,9 @@
 #include "esp_wifi.h"
 #include "esp_now.h"
 #include "nvs_flash.h"
+#include "driver/uart.h"
+#include "esp_system.h"
+#include "string.h"
 
 //Pin configuration
 #define Red_LED GPIO_NUM_46 //Red LED, ON = LOW
@@ -28,20 +31,36 @@
 #define CONFIG_ESPNOW_CHANNEL 5
 static uint8_t broadcastAddress[ESP_NOW_ETH_ALEN] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
 
+//Device Role status
+#define DEVICE_NOT_INITALIZED 0
+#define DEVICE_IS_TIMER_SYNC_MASTER 1
+#define DEVICE_IS_NOT_TIMER_SYNC_MASTER 2
+#define DEVICE_IS_IR_RECEIVER 3
+
 //Pin select
 #define LED_OUTPUT_PIN_SEL  ((1ULL<<Green_LED) | (1ULL<<Blue_LED) | (1ULL<<Red_LED) | (1ULL<<Yellow_LED) | (1ULL<<LOW_IR_LED))
-#define GPIO_INTERRUPT_PIN_SEL  ((1ULL<<Test_button))
-#define ESP_INTR_FLAG_DEFAULT 0
+#define GPIO_INPUT_PIN_SEL  ((1ULL<<Test_button))
+
+//UART
+static const int RX_BUF_SIZE = 1024;
+#define TXD_PIN (GPIO_NUM_43) //TX (Arduino Nano ESP32 Layout)
+#define RXD_PIN (GPIO_NUM_44) //RX (Arduino Nano ESP32 Layout)
 
 //Meta data
 #define SYNC_DEVICES 0
 #define TIME_MEASURE 1
 
-//Global
+//LED Indicator
+#define RGB_on 0
+#define RGB_off 1
+volatile bool one_shot = 0;
+volatile bool LED_Indicator_Arr [6] = {1,1,1,1,1,1};
+
+//Global Variables
 static const char *TAG = "LB_device_A";
-volatile bool IR_test_break = false;
 uint64_t sync_seconds = 5;
-volatile bool state_red_LED = false;
+volatile char device_role = 0;
+
 
 typedef struct PayLoad {
     char meta_data;
@@ -76,16 +95,16 @@ gpio_config_t led_io_conf = {
     ESP_LOGI(TAG, "GPIO LED config - done");
 }
 
-void GPIO_interrupt_config (void){
-    gpio_config_t inter_io_conf = {
-    .intr_type = GPIO_INTR_ANYEDGE,
+void GPIO_input_config (void){
+    gpio_config_t input_io_conf = {
+    .intr_type = GPIO_INTR_DISABLE,
     .mode = GPIO_MODE_INPUT,
-    .pin_bit_mask = GPIO_INTERRUPT_PIN_SEL,
+    .pin_bit_mask = GPIO_INPUT_PIN_SEL,
     .pull_down_en = 0,
     .pull_up_en = 0
     };
-    gpio_config(&inter_io_conf);
-    ESP_LOGI(TAG, "GPIO interrupt config - done");
+    gpio_config(&input_io_conf);
+    ESP_LOGI(TAG, "GPIO input config - done");
 }
 
 void RMT_IR_Signal (void){
@@ -137,21 +156,20 @@ ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK( esp_wifi_set_protocol(ESPNOW_WIFI_IF, WIFI_PROTOCOL_11B|WIFI_PROTOCOL_11G|WIFI_PROTOCOL_11N|WIFI_PROTOCOL_LR) );
 }
 
-static void IRAM_ATTR Test_button_interrupt_isr_handler(void* arg)
+void sendData_uart(const char* data)
 {
-IR_test_break = true;
-gpio_set_level (LOW_IR_LED, 1); //OFF
+    const int len = strlen(data);
+    const int txBytes = uart_write_bytes(UART_NUM_1, data, len);
+    ESP_LOGI(TAG, "Wrote %d bytes", txBytes);
 }
 
-static bool IRAM_ATTR timer_1ms_on_alarm(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_data)
+static bool IRAM_ATTR timer_5s_on_alarm(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_data)
 {
 BaseType_t high_task_awoken = pdFALSE;
 myPayLoad.meta_data = SYNC_DEVICES;
 myPayLoad.intVal_ms = sync_seconds;
 esp_now_send(broadcastAddress, (uint8_t *) &myPayLoad, sizeof(myPayLoad));
 sync_seconds = sync_seconds + 5;
-gpio_set_level(Red_LED, state_red_LED);
-state_red_LED = !state_red_LED;
 return (high_task_awoken == pdTRUE);// return whether we need to yield at the end of ISR
 }
 
@@ -162,39 +180,43 @@ ESP_LOGI(TAG, "Sync sending - done");
 
 static void example_espnow_recv_cb(const esp_now_recv_info_t *recv_info, const uint8_t *data, int len)
 {
+    char text[20];
     memcpy(&myPayLoad, data, sizeof(myPayLoad));
-    if (myPayLoad.meta_data == TIME_MEASURE){
+
+    switch(myPayLoad.meta_data){
+        case TIME_MEASURE:
+        sprintf(text, "%i", myPayLoad.intVal_ms); //rework 
+        strcat(text, "\n"); //rework
+        sendData_uart (text); //rework
         ESP_LOGI(TAG, "MS since started the Timer %i", myPayLoad.intVal_ms);
+        break;
+        case SYNC_DEVICES:
+        device_role = DEVICE_IS_NOT_TIMER_SYNC_MASTER;
+        break;
     }
 }
 
-void app_main(void)
-{
-GPIO_config();
-GPIO_interrupt_config();
-gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT); //Install interrupt isr service routinne
-gpio_isr_handler_add(Test_button, Test_button_interrupt_isr_handler, (void*) Test_button); //Hook isr handler for Test_button gpio pin
-RMT_IR_Signal();
-
+void timer_1ms_config (){
 //Config Timer
-gptimer_handle_t gptimer_1ms = NULL;
+gptimer_handle_t gptimer_5s = NULL;
 gptimer_config_t timer_config = { 
     .clk_src = GPTIMER_CLK_SRC_XTAL,
     .direction = GPTIMER_COUNT_UP,
     .resolution_hz = 1000000,};
-ESP_ERROR_CHECK(gptimer_new_timer(&timer_config, &gptimer_1ms));
+ESP_ERROR_CHECK(gptimer_new_timer(&timer_config, &gptimer_5s));
 gptimer_event_callbacks_t cbs = {
-    .on_alarm = timer_1ms_on_alarm, };
-ESP_ERROR_CHECK(gptimer_register_event_callbacks(gptimer_1ms, &cbs, NULL)); // Check if it is allowed to put the NULL pointer as parameter
-ESP_ERROR_CHECK(gptimer_enable(gptimer_1ms));
-gptimer_alarm_config_t alarm_config_1ms = { 
+    .on_alarm = timer_5s_on_alarm, };
+ESP_ERROR_CHECK(gptimer_register_event_callbacks(gptimer_5s, &cbs, NULL)); // Check if it is allowed to put the NULL pointer as parameter
+ESP_ERROR_CHECK(gptimer_enable(gptimer_5s));
+gptimer_alarm_config_t alarm_config_5s = { 
     .reload_count = 0,
     .alarm_count = 5000000, 
     .flags.auto_reload_on_alarm = true,};   
-ESP_ERROR_CHECK(gptimer_set_alarm_action(gptimer_1ms, &alarm_config_1ms));
-ESP_ERROR_CHECK(gptimer_start(gptimer_1ms));
+ESP_ERROR_CHECK(gptimer_set_alarm_action(gptimer_5s, &alarm_config_5s));
+ESP_ERROR_CHECK(gptimer_start(gptimer_5s));
+}
 
-//Config ESP_Now
+void ESPnow_config (){
 ESP_LOGI(TAG, "Wifi Config");
 esp_err_t ret = nvs_flash_init();
 if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
@@ -210,15 +232,138 @@ memcpy(&peerInfo.peer_addr, broadcastAddress, 6);
 if(!esp_now_is_peer_exist(broadcastAddress))
 {
     esp_now_add_peer(&peerInfo);}
-
-// Main Loop
-while (1) {
-    if (IR_test_break) {
-        vTaskDelay(10 / portTICK_PERIOD_MS); 
-        gpio_set_level (LOW_IR_LED, 0); //ON
-        IR_test_break = false;
-        ESP_LOGI(TAG, "Test break - done");
-    } 
-    vTaskDelay(1000 / portTICK_PERIOD_MS); 
 }
+
+void uart_init(void)
+{
+    const uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+    // Buffer not used for sending data.
+    uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART_NUM_1, &uart_config);
+    uart_set_pin(UART_NUM_1, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+}
+
+static void uart_rx_task(void *arg)
+{
+    static const char *RX_TASK_TAG = "RX_TASK";
+    esp_log_level_set(RX_TASK_TAG, ESP_LOG_INFO);
+    uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE + 1);
+    while (1) {
+        const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, 1000 / portTICK_PERIOD_MS);
+        if (rxBytes > 0) {
+            data[rxBytes] = 0;
+            ESP_LOGI(RX_TASK_TAG, "Read %d bytes: '%s'", rxBytes, data);
+            ESP_LOG_BUFFER_HEXDUMP(RX_TASK_TAG, data, rxBytes, ESP_LOG_INFO);
+            switch (data[0]){
+                case 1: 
+                ESP_LOGI(RX_TASK_TAG, "Start as Timer Sync Master"); 
+                device_role = DEVICE_IS_TIMER_SYNC_MASTER;
+                break;
+            }
+        }
+    }
+    free(data);
+}
+
+void Button_Task(void *params){
+    
+bool button_activated = 1;
+while (1){
+if (gpio_get_level(Test_button)){ 
+    if (button_activated) {
+        button_activated = 0;
+        gpio_set_level (LOW_IR_LED, 1); //IR LED deactivated
+        vTaskDelay(100 / portTICK_PERIOD_MS);
+        gpio_set_level (LOW_IR_LED, 0); //IR LED activated
+    }
+} else {
+
+    button_activated = 1;
+}
+vTaskDelay(100 / portTICK_PERIOD_MS);
+}
+}
+
+void LED_Indicator_Task(void *params) {
+char count = 0;
+
+while (1){
+
+if (one_shot){
+    gpio_set_level (Red_LED, 0); 
+    gpio_set_level (Green_LED, 1); 
+    gpio_set_level (Blue_LED, 1);
+    vTaskDelay(250 / portTICK_PERIOD_MS);
+    count = 11;
+    one_shot = 0;
+}
+
+switch (count) {
+    case 1:
+    gpio_set_level (Red_LED, LED_Indicator_Arr[0]); 
+    gpio_set_level (Green_LED, LED_Indicator_Arr[1]); 
+    gpio_set_level (Blue_LED, LED_Indicator_Arr[2]);
+    break;
+    case 11:
+    gpio_set_level (Red_LED, LED_Indicator_Arr[3]); 
+    gpio_set_level (Green_LED, LED_Indicator_Arr[4]); 
+    gpio_set_level (Blue_LED, LED_Indicator_Arr[5]); 
+    break;
+    case 20:
+    count = 0;
+    break;
+}
+vTaskDelay(100 / portTICK_PERIOD_MS);
+count++;
+}
+}
+
+void app_main(void)
+{
+
+TaskHandle_t task_handle = NULL;
+
+GPIO_config();
+GPIO_input_config();
+RMT_IR_Signal();
+ESPnow_config();
+uart_init();
+
+LED_Indicator_Arr [0] = RGB_off; LED_Indicator_Arr [1] = RGB_off; LED_Indicator_Arr [2] = RGB_on;
+LED_Indicator_Arr [3] = RGB_off; LED_Indicator_Arr [4] = RGB_off; LED_Indicator_Arr [5] = RGB_off;
+xTaskCreatePinnedToCore(LED_Indicator_Task, "LED_Indicator_Task", 2048, NULL, 1, NULL, 0);
+
+xTaskCreatePinnedToCore(uart_rx_task, "uart_rx_task", 1024 * 2, NULL, 2, &task_handle, 0);
+vTaskDelay(5500 / portTICK_PERIOD_MS); //Wait till a sync signal is received
+
+while (device_role == DEVICE_NOT_INITALIZED) {
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+}
+
+vTaskDelete(task_handle);
+
+switch (device_role)
+{
+case DEVICE_IS_TIMER_SYNC_MASTER:
+    timer_1ms_config();
+    LED_Indicator_Arr [0] = RGB_on; LED_Indicator_Arr [1] = RGB_on; LED_Indicator_Arr [2] = RGB_on;
+    LED_Indicator_Arr [3] = RGB_on; LED_Indicator_Arr [4] = RGB_on; LED_Indicator_Arr [5] = RGB_on;
+    ESP_LOGI(TAG, "Device is Timer Sync Master");
+    break;
+case DEVICE_IS_NOT_TIMER_SYNC_MASTER:
+    ESP_LOGI(TAG, "Device is NOT Timer Sync Master");
+    LED_Indicator_Arr [0] = RGB_off; LED_Indicator_Arr [1] = RGB_on; LED_Indicator_Arr [2] = RGB_off;
+    LED_Indicator_Arr [3] = RGB_off; LED_Indicator_Arr [4] = RGB_on; LED_Indicator_Arr [5] = RGB_off;
+    break;
+}
+
+xTaskCreatePinnedToCore(Button_Task, "Button_Task", 2048, NULL, 1, NULL, 0);
+
 }
